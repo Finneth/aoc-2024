da50618b0d89846a44e426fbb81fc6f5b9009936
Finn McDonnell <finneth@gmail.com>
2024-12-08 23:25:39 +0000

    Breaking down instruction set.

--- day3/prog.c
+++ day3/prog.c
@@ -46,7 +46,124 @@ PairNode * AddNode(PairNode *current, int a, int b) {
     return next;
 }
 
-PairNode *LoadTestCaseInputData(char filePath[])
+struct StringNode {
+    int len;
+    StringNode * next;
+    char string[];
+};
+
+void FreeStringNode(StringNode* p){
+    StringNode* next = p->next;
+
+    if( next == NULL )
+    {
+        return;
+    }
+
+    FreeStringNode(next);
+    free(p);
+}
+
+void PrintStringNode(StringNode* p){
+    StringNode* current = p;
+    int item = 1;
+
+    printf("StringNode\n");
+    while(current != NULL){
+        printf("%d\t%s\n", item, current->string);
+        current = current->next;
+        item++;
+    }
+}
+
+StringNode * AddStringNode(StringNode *current, char string[]) {
+    struct StringNode* next = (StringNode*) malloc(sizeof(*next) + sizeof(char) * (strlen(string) + 1));
+    if (next == NULL) {
+        perror("Failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+
+    next->len = strlen(string) + 1;
+    next->next = NULL;
+    strcpy(next->string, string);
+
+    if (current == NULL){
+        return next;
+    }
+
+    current->next = next;
+    return next;
+}
+
+// Match returns a StringNode, representing the matches of each string in
+// the supplied StringNode, when the regex string supplied is applied to it.
+StringNode *Match(StringNode * stringNode, char regexString[])
+{
+    StringNode *readCurrent = stringNode;
+    size_t maxGroups = 30;
+    size_t maxMatches = 10000;
+
+    regex_t regexCompiled;
+    regmatch_t groupArray[maxGroups];
+    unsigned int m;
+    char * cursor;
+
+    int err = regcomp(&regexCompiled, regexString, REG_EXTENDED);
+
+    if (err)
+    {
+        printf("Could not compile regular expression. %d\n", err);
+        return NULL;
+    };
+
+
+    StringNode *first;
+    StringNode *writeCurrent;
+
+    m = 0;
+    cursor = readCurrent->string;
+    for (m = 0; m < maxMatches; m ++)
+    {
+        if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0)) {
+            break;  // No more matches
+        }
+        
+
+        unsigned int start = 1;
+        unsigned int g = start;
+        unsigned int offset = 0;
+        for (g = start; g < maxGroups; g++)
+        {
+            if (groupArray[g].rm_so == (size_t)-1) {
+                break;  // No more groups    
+            }
+
+            if (g == start) {
+                offset = groupArray[g].rm_eo;    
+            }      
+
+            char cursorCopy[strlen(cursor) + 1];
+            strcpy(cursorCopy, cursor);
+            cursorCopy[groupArray[g].rm_eo] = 0;
+
+            writeCurrent = AddStringNode(writeCurrent, cursorCopy + groupArray[g].rm_so);
+
+            if (m == 0 && g == start) {
+                first = writeCurrent;
+            }
+        }
+        cursor += offset;
+    }
+
+    regfree(&regexCompiled); 
+
+    return first;
+}
+
+
+// LoadTestCaseInputData Loads the data in filePath into a 
+// StringNode linked list, with each line occupying a separate node
+StringNode *LoadTestCaseInputData(char filePath[])
 {
     FILE *testCaseFile = fopen(filePath, "r");
     if (testCaseFile == NULL) {
@@ -57,66 +174,14 @@ PairNode *LoadTestCaseInputData(char filePath[])
     char * line = NULL;
     size_t len = 0;
 
-    int lineNumber = 0;
+    size_t size = getline(&line, &len, testCaseFile);
 
-    PairNode * first;
-    PairNode * current;
+    StringNode * first = AddStringNode(NULL, line);
+    StringNode * current = first;
 
-    bool firstRun = true;
-
-    while(getline(&line, &len, testCaseFile) != -1){
-        char regexString[] = "mul\\(([0-9]+),([0-9]+)\\)";
-        size_t maxGroups = 3;
-        size_t maxMatches = 10000;
-
-        regex_t regexCompiled;
-        regmatch_t groupArray[maxGroups];
-        unsigned int m;
-        char * cursor;
-
-        if (regcomp(&regexCompiled, regexString, REG_EXTENDED))
-        {
-            printf("Could not compile regular expression.\n");
-            return NULL;
-        };
-
-        m = 0;
-        cursor = line;
-        for (m = 0; m < maxMatches; m ++)
-        {
-            if (regexec(&regexCompiled, cursor, maxGroups, groupArray, 0)){
-                break;  // No more matches
-            }
-
-            // Copy the line, but put a string terminator in at the match end index
-            // for match 1, which corresponds to the first match.
-            char cursorCopy1[strlen(cursor) + 1];
-            strcpy(cursorCopy1, cursor);
-            cursorCopy1[groupArray[1].rm_eo] = 0;
-            
-            // Copy the line, but put a string terminator in at the match end index
-            // for match 2, which corresponds to the second match.
-            char cursorCopy2[strlen(cursor) + 1];
-            strcpy(cursorCopy2, cursor);
-            cursorCopy2[groupArray[2].rm_eo] = 0;
-
-            // Convert the values to ints
-            int a = atoi(cursorCopy1 + groupArray[1].rm_so);
-            int b = atoi(cursorCopy2 + groupArray[2].rm_so);
-
-            if(firstRun) {
-                first = AddNode(NULL, a, b);
-                current = first;
-                firstRun = false;
-            } else {
-                current = AddNode(current, a, b);
-            }
-            
-            cursor += groupArray[0].rm_eo;
-        }
-
-        regfree(&regexCompiled); 
-    }
+    while(getline(&line, &len, testCaseFile) != -1) {
+        current = AddStringNode(first, line);
+    } 
 
     if (line) {
         free(line);
@@ -136,7 +201,8 @@ struct TestInfo {
     TestResults expectedValues;
 };
 
-void GetTestInfo(TestInfo *testInfo, char testCaseInfoFilePath[]) {
+void GetTestInfo(TestInfo *testInfo, char testCaseInfoFilePath[])
+{
     FILE *testCaseFile = fopen(testCaseInfoFilePath, "r");
     if (testCaseFile == NULL) {
         printf("no such file.\n");
@@ -150,12 +216,14 @@ void GetTestInfo(TestInfo *testInfo, char testCaseInfoFilePath[]) {
     fclose(testCaseFile); 
 }
 
-void constructTestFilePath(char fileName[], char path[]) {
+void constructTestFilePath(char fileName[], char path[])
+{
     strcat(path, testFilePrefix);
     strcat(path, fileName);  
 }
 
-void checkResults(TestInfo testInfo, TestResults expectedValues) {    
+void checkResults(TestInfo testInfo, TestResults expectedValues)
+{    
     if (testInfo.expectedValues.part1 != expectedValues.part1) {
         printf("%s FAILED. part1: Expected %ld, got %ld\n\n", testInfo.testCaseDataFileName, testInfo.expectedValues.part1, expectedValues.part1);
     }
@@ -172,22 +240,23 @@ void PrintTestCase(char filePath[], int part1, int part2)
     printf("Part 2:\t%d\n", part2);
 }
 
-long CalcTotal(PairNode* p){
+long CalcTotal(PairNode* p)
+{
     PairNode* current = p;
     long acc = 0;
 
-    while(current != NULL){
+    while(current != NULL) {
         acc += current->pair.a * current->pair.b;
         current = current->next;
     }
     return acc;
 }
 
-void FreePairNode(PairNode* p){
+void FreePairNode(PairNode* p)
+{
     PairNode* next = p->next;
 
-    if( next == NULL )
-    {
+    if( next == NULL ) {
         return;
     }
 
@@ -198,9 +267,10 @@ void FreePairNode(PairNode* p){
 int main()
 {
     char testCaseInfoFileNames[][MAX_FILE_NAME] = {
-        "testCase1.txt",
-        "testCase2.txt",
-        "testCase3.txt",
+        // "testCase1.txt",
+        // "testCase2.txt",
+        // "testCase3.txt",
+        "testCase4.txt",
     };
 
     for (size_t i = 0; i < sizeof(testCaseInfoFileNames) / sizeof(testCaseInfoFileNames[0]); i++)
@@ -216,34 +286,40 @@ int main()
         char testCaseDataFilePath[MAX_FILE_NAME] = "";
         constructTestFilePath(testInfo.testCaseDataFileName, testCaseDataFilePath);
 
-        PairNode* first = LoadTestCaseInputData(testCaseDataFilePath);
+        StringNode* first = LoadTestCaseInputData(testCaseDataFilePath);
 
-        if (DEBUG) {
-            PrintPairNode(first);
-        }
+        char instructionRegex[] = "(mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\))";
+        StringNode* instructions = Match(first, instructionRegex);
+        PrintStringNode(instructions);
+
+        // FHM: A second regex to test which type of instruction we have
+
+        // if (DEBUG) {
+        //     PrintPairNode(first);
+        // }
 
 
-        TestResults results;
+        // TestResults results;
 
-        // Calculate results
-        results.part1 = CalcTotal(first);
-        results.part2 = 0;
+        // // Calculate results
+        // results.part1 = CalcTotal(first);
+        // results.part2 = 0;
 
-        // Debug logging for troubleshooting
-        if (DEBUG) {
-            PrintTestCase(testCaseDataFilePath, results.part1, results.part2);    
-            if (testInfo.expectedValues.part1 == results.part1) {
-                printf("%s SUCCESS. part1: Expected %ld, got %ld\n\n", testInfo.testCaseDataFileName, testInfo.expectedValues.part1, results.part1);
-            }
-            if (testInfo.expectedValues.part2 == results.part2) {
-                printf("%s SUCCESS. part2: Expected %ld, got %ld\n\n", testInfo.testCaseDataFileName, testInfo.expectedValues.part2, results.part2);
-            }
-        }
+        // // Debug logging for troubleshooting
+        // if (DEBUG) {
+        //     PrintTestCase(testCaseDataFilePath, results.part1, results.part2);    
+        //     if (testInfo.expectedValues.part1 == results.part1) {
+        //         printf("%s SUCCESS. part1: Expected %ld, got %ld\n\n", testInfo.testCaseDataFileName, testInfo.expectedValues.part1, results.part1);
+        //     }
+        //     if (testInfo.expectedValues.part2 == results.part2) {
+        //         printf("%s SUCCESS. part2: Expected %ld, got %ld\n\n", testInfo.testCaseDataFileName, testInfo.expectedValues.part2, results.part2);
+        //     }
+        // }
         
-        // Assert against expections
-        checkResults(testInfo, results);
+        // // Assert against expections
+        // checkResults(testInfo, results);
 
-        FreePairNode(first);
+        FreeStringNode(first);
         
     }
     return 0;
